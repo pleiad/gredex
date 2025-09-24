f0b3325ab67ec2951a73e81e20c2d98689e62db3
matia storo <mtoro.cl@gmail.com>
2025-09-24 14:56:04 -0300

    base path for vite

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,16 +1,24 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
 
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 // https://vite.dev/config/
-export default defineConfig({
-  base: "./",
-  plugins: [react()],
-  server: {
-    proxy: {
-      "/api": {
-        target: "http://localhost:8080",
-        changeOrigin: true,
+export default defineConfig(({ mode }) => {
+  const envDir = path.resolve(__dirname, ".."); // parent folder of frontend
+  const env = loadEnv(mode, envDir, "");
+  console.log("Vite env:", env.VITE_BASE);
+  return {
+    base: env.VITE_BASE || "/",
+    plugins: [react()],
+    server: {
+      proxy: {
+        "/api": {
+          target: "http://localhost:8080",
+          changeOrigin: true,
+        },
       },
     },
-  },
+  };
 });
